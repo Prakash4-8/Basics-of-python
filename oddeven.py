0319a078340782311ecdd88b46fe143afea8c57a
SiddharthSinghKushwaha <siddharthsk211@gmail.com>
2021-12-29 22:43:12 +0530

    Input is taken from user

--- oddeven.py
+++ oddeven.py
@@ -1,6 +1,7 @@
-x=87687
-if(x%2==0):
-    print("Even")
-else:
-    print("Odd")
+# How to take input from user
+num = int(input('Enter a number to check: '))   # int is to used to convert the string data type into int
 
+if num % 2 == 0:
+    print(num, " is a Even number")
+else:
+    print(num, " is a Odd number")
